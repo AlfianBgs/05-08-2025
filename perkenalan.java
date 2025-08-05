ae8e6a829a057cc0b1aabd38602d462562343c9d
AlfianBagasFirzatullah <alfianrizaltullah@gmail.com>
2025-08-05 12:48:31 +0700

    Add files via upload
    perkenalan dan mengenali nama 5 teman dan asal mereka dengan java

---
+++ perkenalan.java
@@ -0,0 +1,103 @@
+public class perkenalan {
+    public static void main(String[] args) {
+
+        //tide_data variabel = "nilai";
+        String firstname = "Alfian ";
+        String lastname = "Bagas Firzatullah";
+        String fullname = firstname + lastname;
+        int usia = 16;
+        String kelas = "X RPL 7";
+        String smp = "SMP Negeri 1 Lumajang";
+        String hobi = "Bermain Game";
+        String cita_cita = "Gamers";
+        System.out.println("Nama : " + fullname);
+        System.out.println("Usia : "+usia + " Tahun");
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Asal SMP : "+smp);
+        System.out.println("Hobi : "+hobi);
+        System.out.println("Cita-cita : "+cita_cita);
+
+System.out.println(" ");
+        String firstname1 = "M Rohman ";
+        String lastname1 = "Muhammad";
+        String fullname1 = firstname1 + lastname1;
+        int usia1 = 15;
+        String kelas1 = "X RPL 7";
+        String smp1 = "SMP Negeri 2 Sumberpucung";
+        String hobi1 = "Running";
+        String cita_cita1 = "Web designer";
+        System.out.println("Nama : " + fullname1);
+        System.out.println("Usia : "+usia1 + " Tahun");
+        System.out.println("Kelas : "+kelas1);
+        System.out.println("Asal SMP : "+smp1);
+        System.out.println("Hobi : "+hobi1);
+        System.out.println("Cita-cita : "+cita_cita1);
+
+System.out.println(" ");
+        String firstname2 = "M. Nasywan ";
+        String lastname2 = "Alfardan";
+        String fullname2 = firstname2 + lastname2;
+        int usia2 = 15;
+        String kelas2 = "X RPL 7";
+        String smp2 = "SMPIT Insan Permata";
+        String hobi2 = "Olahraga";
+        String cita_cita2 = "Pengusaha";
+        System.out.println("Nama : " + fullname2);
+        System.out.println("Usia : "+usia2 + " Tahun");
+        System.out.println("Kelas : "+kelas2);
+        System.out.println("Asal SMP : "+smp2);
+        System.out.println("Hobi : "+hobi2);
+        System.out.println("Cita-cita : "+cita_cita2);
+
+System.out.println(" ");
+        String firstname3 = "Grisli ";
+        String lastname3 = "Billywanto";
+        String fullname3 = firstname3 + lastname3;
+        int usia3 = 15;
+        String kelas3 = "X RPL 7";
+        String smp3 = "SMP Negeri 4 Kepanjen";
+        String hobi3 = "Belajar";
+        String cita_cita3 = "Data Analyst";
+        System.out.println("Nama : " + fullname3);
+        System.out.println("Usia : "+usia3 + " Tahun");
+        System.out.println("Kelas : "+kelas3);
+        System.out.println("Asal SMP : "+smp3);
+        System.out.println("Hobi : "+hobi3);
+        System.out.println("Cita-cita : "+cita_cita3);
+
+System.out.println(" ");
+       String firstname4 = "Cendika ";
+       String lastname4 = "Fikara ";
+       String fullname4 = firstname4 + lastname4;
+       int usia4 = 16;
+       String kelas4 = "X RPL 7";
+       String smp4 = "MTs Khadijah Malang";
+       String hobi4 = "Main Game";
+       String cita_cita4 = "Software Enginer";
+       System.out.println("Nama : " + fullname4);
+       System.out.println("Usia : "+usia4 + " Tahun");
+       System.out.println("Kelas : "+kelas4);
+       System.out.println("Asal SMP : "+smp4);
+       System.out.println("Hobi : "+ hobi4);
+       System.out.println("Cita-cita : "+cita_cita4);
+
+System.out.println(" ");
+       String firstname5 = "Zaki ";
+       String lastname5 = "Khoirudin Haflah ";
+       String fullname5 = firstname4 + lastname4;
+       int usia5 = 15;
+       String kelas5 = "X RPL 7";
+       String smp5 = "SMP Negeri 1 Pakis";
+       String hobi5 = "Mancing";
+       String cita_cita5 = "Jadi Orang Kaya";
+       System.out.println("Nama : " + fullname4);
+       System.out.println("Usia : "+usia4 + " Tahun");
+       System.out.println("Kelas : "+kelas4);
+       System.out.println("Asal SMP : "+smp4);
+       System.out.println("Hobi : "+ hobi4);
+       System.out.println("Cita-cita : "+cita_cita4); 
+    }
+    
+}
+    
+
